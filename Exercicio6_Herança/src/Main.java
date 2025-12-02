a354d9fd7aa2d65c0a88b67a4ab9d1ec570acbec
Davy Oliveira Melo <davymelo1403@gmail.com>
2025-12-02 02:25:38 -0300

    Classe "Main" para testar

--- Exercicio6_Herança/src/Main.java
+++ Exercicio6_Herança/src/Main.java
@@ -1,3 +1,23 @@
 public class Main {
+    public static void main(String[] args) {
+        
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
     
+        p1.setNome("Davy");
+        p2.setNome("Pedro");
+        p3.setNome("Samara");
+        p4.setNome("Filipe");
+
+        p2.setCurso("Computacao");
+        p3.setSalario(10000.75f);
+        p4.setSetor("Coordenador");
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+    }
 }
