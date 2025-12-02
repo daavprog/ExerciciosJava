81d19d680eac62dc1a860b873fdb769e0a319a0e
Davy Oliveira Melo <davymelo1403@gmail.com>
2025-12-02 03:59:25 -0300

    Implemento array para teste

--- Exercicio6_heranca_Q4/src/TesteVeiculos.java
+++ Exercicio6_heranca_Q4/src/TesteVeiculos.java
@@ -1,3 +1,15 @@
 public class TesteVeiculos {
-    
+    public static void main(String[] args) {
+        // Crio um ARRAY de Veiculos (Polimorfismo)
+        // O array é do tipo genérico, mas aceita os filhos
+        Veiculo[] garagem = new Veiculo[3];
+
+        garagem[0] = new Carro("Toyota", "Corolla", 4);
+        garagem[1] = new Moto("Honda", "CB 500", 500);
+        garagem[2] = new Carro("Fiat", "Uno", 2);
+
+        for (Veiculo v : garagem) {
+            v.exibirInfo();
+        }
+    }
 }
